a6d09dd3ff0e95d08ef03806f92ac7740e846200
Lucas Mobile <lucas.mobileit@outlook.com>
2024-07-30 15:49:02 -0300

    update-4

---
+++ ws-dio/src/application/Usuario.java
@@ -0,0 +1,43 @@
+package application;
+
+public class Usuario {
+
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal: " + smartTv.canal);
+        System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.mudarCanal(5);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("Canal atual: " + smartTv.canal);
+    }
+    
+}
