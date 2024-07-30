efd7c7effa26b314e8c3a6e42819e8f7302093e5
Lucas Mobile <lucas.mobileit@outlook.com>
2024-07-30 14:33:25 -0300

    update-3

--- ws-dio/src/application/app.java
+++ ws-dio/src/application/app.java
@@ -1,9 +1,32 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class app {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        String name;
+        double price;
+        int quantity;
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        name = sc.nextLine();
+        System.out.print("Price: ");
+        price = sc.nextDouble();
+        System.out.print("Quantity: ");
+        quantity = sc.nextInt();
+
+
+        double totalValueInStock = price * quantity;
+
+        System.out.println("Product data: " + name + ", " + price + ", " + quantity);
+
+        System.out.println("Total: " + totalValueInStock);
     }
 
 }
